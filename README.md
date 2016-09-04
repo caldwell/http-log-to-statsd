c4d09282deeb05baf9866ce8ef551ffde9d8188a
David Caldwell <david@porkrind.org>
2016-09-03 21:06:15 -0700

    Remove --suffix options and get suffix from end of log line instead
    This way the web server can share log lines and log files for multiple hosts
    more easily. Thanks, @ibrezac, for the suggestion.

--- README.md
+++ README.md
@@ -14,24 +14,45 @@ server are:
 - `http.request.requests`: count of requests
 
 The `http.request` prefix can by changed with the `--prefix` command line
-option. In addition, the `--suffix` options may be used to add arbitrary
-suffixes to the stat names. For instance, telgraf/influx users might want to
-add `",sometag=somevalue"` to inject custom tags.
+option. In addition, the incoming log line can specify a suffix in the 7th
+field, which may be used to add arbitrary suffixes to the stat names. For
+instance, telgraf/influx users might want to add `",sometag=somevalue"` to
+inject custom tags.
 
 ## Configuring nginx:
 
     log_format stats '$scheme $request_method $status $request_length $body_bytes_sent $request_time';
     access_log syslog:server=127.0.0.1:6666 stats;
 
+To add an arbitrary suffix to each metric name, add one more field to the
+log_format:
+
+    log_format stats '$scheme $request_method $status $request_length $body_bytes_sent $request_time .$host';
+
+If you use Telegraf/InfluxDB, you might want to use InfluxDB's tag format:
+
+    log_format stats '$scheme $request_method $status $request_length $body_bytes_sent $request_time ,host=$host';
+
 ## Configuring apache 2.4 and later (requires netcat to be installed):
 
     # https method status request-size(bytes) response-size(bytes) request-time(ms)
-    LogFormat "%{REQUEST_SCHEME}x %m %>s %I %O %{ms}T" stats
+    LogFormat "%{REQUEST_SCHEME}x %m %>s %I %O %{ms}T"
     CustomLog "|/bin/nc -u localhost 6666" stats
+
+To add an arbitrary suffix to each metric name, add one more field to the
+LogFormat:
+
+    # https method status request-size(bytes) response-size(bytes) request-time(ms) hostname/extra-suffix
+    LogFormat "%{REQUEST_SCHEME}x %m %>s %I %O %{ms}T .%v"
+
+If you use Telegraf/InfluxDB, you might want to use InfluxDB's tag format:
+
+    # https method status request-size(bytes) response-size(bytes) request-time(ms) hostname/extra-suffix
+    LogFormat "%{REQUEST_SCHEME}x %m %>s %I %O %{ms}T ,host=%v"
 
 ## Usage:
 
-      http-log-to-statsd [-h | --help] [-v...] [--listen=<listen>] [--statsd=<server>] [--prefix=<prefix>] [--suffix=<suffix>]
+      http-log-to-statsd [-h | --help] [-v...] [--listen=<listen>] [--statsd=<server>] [--prefix=<prefix>]
 
 ### Options:
 
@@ -39,4 +60,3 @@ add `",sometag=somevalue"` to inject custom tags.
       --listen=<listen>        Address and port number to listen on [default: 127.0.0.1:6666]
       --statsd=<server>        Address and port number of statsd server [default: 127.0.0.1:8125]
       --prefix=<prefix>        Statsd prefix for metrics [default: "http.request"]
-      --suffix=<suffix>        Statsd suffix for metrics [default: ""]
--- http-log-to-statsd.rs
+++ http-log-to-statsd.rs
@@ -13,13 +13,12 @@ struct Options {
     flag_listen: String,
     flag_statsd: String,
     flag_prefix: String,
-    flag_suffix: String,
 }
 
 fn main() {
         let usage = format!("
 Usage:
-  http-log-to-statsd [-h | --help] [-v...] [--listen=<listen>] [--statsd=<server>] [--prefix=<prefix>] [--suffix=<suffix>]
+  http-log-to-statsd [-h | --help] [-v...] [--listen=<listen>] [--statsd=<server>] [--prefix=<prefix>]
 
 Options:
   -h --help                Show this screen.
@@ -27,7 +26,6 @@ Options:
   --listen=<listen>        Address and port number to listen on [default: 127.0.0.1:6666]
   --statsd=<server>        Address and port number of statsd server [default: 127.0.0.1:{}]
   --prefix=<prefix>        Statsd prefix for metrics [default: http.request]
-  --suffix=<suffix>        Statsd suffix for metrics [default: ]
 ", DEFAULT_PORT);
 
     let options: Options = Docopt::new(usage)
@@ -39,7 +37,6 @@ Options:
     let verbose = options.flag_v;
 
     let statsd = StatsdClient::<UdpMetricSink>::from_udp_host(options.flag_prefix.as_str(), options.flag_statsd.as_str()).unwrap();
-    let name = |name: &str| { format!("{}{}", name, options.flag_suffix) };
 
     // Read from webserver and accumulate stats
     let socket = UdpSocket::bind(options.flag_listen.as_str()).unwrap();
@@ -48,16 +45,19 @@ Options:
         if let Ok((amt, _/*src*/)) = socket.recv_from(&mut buf) {
             if let Ok(line) = std::str::from_utf8(&buf[0..amt]).map(|s| s.to_string()) {
                 if verbose > 1 { println!("{}", line) }
-                // <190>Sep  3 15:40:50 deck nginx: http GET 200 751 498 0.042
+                // <190>Sep  3 15:40:50 deck nginx: http GET 200 751 498 0.042 extra.suffix
                 let line = if line.len() > 1 && line.chars().nth(0).unwrap() == '<' { // Strip off syslog gunk, if it exists
                     if let Some(start_byte) = line.find(": http").map(|l|l+2) {
                         std::str::from_utf8(&line.as_bytes()[start_byte..]).unwrap_or(line.as_str()).to_string()
                     } else { line }
                 } else { line };
                 let fields: Vec<&str> = line.split_whitespace().collect();
-                if fields.len() == 6 {
-                    let (scheme, method, status, request_bytes, response_bytes, response_time_ms) = (fields[0], fields[1].to_lowercase(), fields[2], fields[3], fields[4], fields[5]);
-                    if verbose > 1 { println!("{},{},{},{},{},{}", scheme, method, status, request_bytes, response_bytes, response_time_ms) }
+                if fields.len() >= 6 {
+                    let empty_string = ""; // seriously, rust?
+                    let (scheme, method, status, request_bytes, response_bytes, response_time_ms, suffix) = (fields[0], fields[1].to_lowercase(), fields[2], fields[3], fields[4], fields[5], fields.get(6).unwrap_or(&empty_string));
+                    if verbose > 1 { println!("{},{},{},{},{},{},{}", scheme, method, status, request_bytes, response_bytes, response_time_ms, suffix) }
+
+                    let name = |name: &str| { [name, suffix].concat() };
 
                     let _ = statsd.incr(&name(&scheme));
                     let _ = statsd.incr(&name(&method));
