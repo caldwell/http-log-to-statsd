dec8bf4637a35b5733201a5bd8699fa6b8d136a4
David Caldwell <david@porkrind.org>
2018-04-19 15:08:46 -0700

    Split parsing off into its own function (for improved testability).

--- http-log-to-statsd.rs
+++ http-log-to-statsd.rs
@@ -41,42 +41,17 @@ Options:
     // Read from webserver and accumulate stats
     let socket = UdpSocket::bind(options.flag_listen.as_str()).unwrap();
     let mut buf = [0; 512];
+    let mut parser = Parser::new();
     loop {
         if let Ok((amt, _/*src*/)) = socket.recv_from(&mut buf) {
             if let Ok(line) = std::str::from_utf8(&buf[0..amt]).map(|s| s.to_string()) {
                 if verbose > 1 { println!("{}", line) }
-                // <190>Sep  3 15:40:50 deck nginx: http GET 200 751 498 0.042 extra.suffix
-                let line = if line.len() > 1 && line.chars().nth(0).unwrap() == '<' { // Strip off syslog gunk, if it exists
-                    if let Some(start_byte) = line.find(": http").map(|l|l+2) {
-                        std::str::from_utf8(&line.as_bytes()[start_byte..]).unwrap_or(line.as_str()).to_string()
-                    } else { line }
-                } else { line };
 
-                let mut suffix = "";
-                let name = |name: &str, suffix: &str| { [name, suffix].concat() };
-                for field in line.split_whitespace() {
-                    if field.len() < 2 { continue }
-                    match field.chars().nth(0).unwrap_or(' ') {
-                        '+' => { /* +GET +200 */ let _ = statsd.incr(&name(&field[1..], suffix)); },
-                        'x' => { /* +GET x200 */ let _ = statsd.incr(&name(&format!("{}xx", field.chars().nth(1).unwrap_or('X')), suffix)); },
-                        '~' => { /* ~request_bytes:501   ~response_time_ms:1.52*1000 */
-                                     let x: Vec<&str> = field[1..].splitn(2, ':').collect();
-                                     if x.len() == 2 {
-                                         let (key, mut value, mut scale) = (x[0], x[1], "1");
-                                         if value.contains("*") {
-                                             let x: Vec<&str> = field[1..].splitn(2, '*').collect();
-                                             value = x[1];
-                                             scale = x[0];
-                                         }
-                                         let _ = statsd.time(&name(key, suffix),
-                                                             if value.contains('.') || scale.contains('.') { (value.parse::<f64>().unwrap_or(0.0) * scale.parse::<f64>().unwrap_or(1.0)) as u64 }
-                                                             else                                          { value.parse::<u64>().unwrap_or(0)    * scale.parse::<u64>().unwrap_or(1)  });
-                                     } else {
-                                         println!("Couldn't parse average(~) field: {}", field)
-                                     }
-                        },
-                        '>' => { suffix = &field[1..] },
-                        _ => { println!("Unknown field: {}", field) }
+                let stats = parser.parse_line(line);
+                for stat in stats {
+                    match stat {
+                        Stat::Incr(name)    => { let _ = statsd.incr(&name); },
+                        Stat::Avg(name,val) => { let _ = statsd.time(&name, val); },
                     }
                 }
             }
@@ -84,3 +59,57 @@ Options:
     }
 }
 
+
+#[derive(Debug, PartialEq)]
+pub enum Stat {
+    Incr(String),
+    Avg(String,u64),
+}
+
+#[derive(Debug)]
+pub struct Parser {
+    suffix: String,
+}
+
+impl Parser {
+    pub fn new() -> Parser {
+        Parser{suffix: "".to_string()}
+    }
+    pub fn parse_line(&mut self, line: String) -> Vec<Stat> {
+        let mut stats = Vec::new();
+        // <190>Sep  3 15:40:50 deck nginx: http GET 200 751 498 0.042 extra.suffix
+        let line = if line.len() > 1 && line.chars().nth(0).unwrap() == '<' { // Strip off syslog gunk, if it exists
+            if let Some(start_byte) = line.find(": http").map(|l|l+2) {
+                std::str::from_utf8(&line.as_bytes()[start_byte..]).unwrap_or(line.as_str()).to_string()
+            } else { line }
+        } else { line };
+        self.suffix = "".to_string();
+        let name = |name: &str, suffix: &str| { [name, suffix].concat() };
+        for field in line.split_whitespace() {
+            if field.len() < 2 { continue }
+            match field.chars().nth(0).unwrap_or(' ') {
+                '+' => { /* +GET +200 */ let _ = stats.push(Stat::Incr(name(&field[1..], &self.suffix))); },
+                'x' => { /* +GET x200 */ let _ = stats.push(Stat::Incr(name(&format!("{}xx", field.chars().nth(1).unwrap_or('X')), &self.suffix))); },
+                '~' => { /* ~request_bytes:501   ~response_time_ms:1.52*1000 */
+                             let x: Vec<&str> = field[1..].splitn(2, ':').collect();
+                             if x.len() == 2 {
+                                 let (key, mut value, mut scale) = (x[0], x[1], "1");
+                                 if value.contains("*") {
+                                     let x: Vec<&str> = field[1..].splitn(2, '*').collect();
+                                     value = x[1];
+                                     scale = x[0];
+                                 }
+                                 let _ = stats.push(Stat::Avg(name(key, &self.suffix),
+                                                              if value.contains('.') || scale.contains('.') { (value.parse::<f64>().unwrap_or(0.0) * scale.parse::<f64>().unwrap_or(1.0)) as u64 }
+                                                              else                                          { value.parse::<u64>().unwrap_or(0)    * scale.parse::<u64>().unwrap_or(1)  }));
+                             } else {
+                                 println!("Couldn't parse average(~) field: {}", field)
+                             }
+                },
+                '>' => { self.suffix = field[1..].to_string() },
+                _ => { println!("Unknown field: {}", field) }
+            }
+        }
+        stats
+    }
+}
